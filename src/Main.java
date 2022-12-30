cb0eae6c8e06b49ba91cebbe1729ea55c1ff7bac
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2022-12-30 22:55:41 +0300

    start

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Поехали!
+        printMenu();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int command = scanner.nextInt();
+            if (command == 1) {
+
+            }
+            else if (command == 2) {
+
+            }
+            else if (command == 3) {
+
+            }
+            else if (command == 4) {
+
+            }
+            else if (command == 5) {
+
+            }
+        }
     }
-}
+
+
+
+
+
+    public static void printMenu (){
+            System.out.println("Что вы хотите сделать? ");
+            System.out.println("1 - Считать все месячные отчёты");
+            System.out.println("2 - Считать годовой отчёт");
+            System.out.println("3 - Сверить отчёты");
+            System.out.println("4 - Вывести информацию о всех месячных отчётах");
+            System.out.println("5 - Вывести информацию о годовом отчёте");
+        }
+    }
+
 
